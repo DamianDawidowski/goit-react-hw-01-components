c60ea45a8ce2a1c82998d542c3acd668048559df
Damian Dawidowski <damiandawidowski@gmail.com>
2023-05-13 20:04:23 +0200

    modified:   src/components/statisticsComponent/Statistics.jsx

--- src/components/statisticsComponent/Statistics.jsx
+++ src/components/statisticsComponent/Statistics.jsx
@@ -8,7 +8,6 @@ const Statistics = ({ stats, title }) => {
   return (
     <section class="statistics">
       {title !== '' && <h2 class="title">{title}</h2>}
-
       <ul class="stat-list">
         {stats.map(({ id, label, percentage }) => {
           return (
